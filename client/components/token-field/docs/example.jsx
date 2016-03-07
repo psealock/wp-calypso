7833ff9bf736042ea40549f6d657e19a9daca2a0
Eric Binnion <ericbinnion@gmail.com>
2016-03-07 17:12:04 -0600

    TokenField: Updates devDocs design example

--- client/components/token-field/docs/example.jsx
+++ client/components/token-field/docs/example.jsx
@@ -27,20 +27,57 @@ var TokenFields = React.createClass( {
 	getInitialState: function() {
 		return {
 			tokenSuggestions: suggestions,
-			tokens: Object.freeze( [ 'foo', 'bar' ] )
+			isBorderless: false,
+			tokens: Object.freeze( [ 'foo', 'bar' ] ),
+			tokensWithStatuses: [
+				{
+					value: 'Success',
+					status: 'success'
+				},
+				{
+					value: 'Error',
+					status: 'error'
+				},
+				{
+					value: 'Validating',
+					status: 'validating'
+				}
+			]
 		};
 	},
 
+	toggleBorderless() {
+		this.setState( { isBorderless: ! this.state.isBorderless } );
+	},
+
 	render: function() {
+		const toggleBorderlessText = this.state.isBorderless ? 'Regular Tokens' : 'Borderless Tokens'
 		return (
 			<div className="design-assets__group">
 				<h2>
 					<a href="/devdocs/design/token-fields">Token Field</a>
+					<a className="design-assets__toggle button" onClick={ this.toggleBorderless }>{ toggleBorderlessText }</a>
 				</h2>
+
+				<p>
+					The <code>TokenField</code> is a field similar to the tags and categories
+					fields in the interim editor chrome, or the "to" field in Mail on OS X.
+					Tokens can be entered by typing them or selecting them from a list of suggested tokens.
+				</p>
+
+				<h3>TokenField with Suggestions</h3>
+
 				<TokenField
+					isBorderless={ this.state.isBorderless }
 					suggestions={ this.state.tokenSuggestions }
 					value={ this.state.tokens }
 					onChange={ this._onTokensChange } />
+
+				<h3 style={ { marginTop: 20 } }>Disabled TokenField with Token Statuses</h3>
+				<TokenField
+					disabled
+					isBorderless={ this.state.isBorderless }
+					value={ this.state.tokensWithStatuses } />
 			</div>
 		);
 	},
